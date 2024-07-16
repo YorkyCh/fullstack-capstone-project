d20ea0256df246ff3fe43eb348bdcd11f4e504f9
YorkyCh <95034701+YorkyCh@users.noreply.github.com>
2024-07-16 20:28:32 +0200

    frontend

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,12 +22,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
 const giftRoutes = require("./routes/giftRoutes"); // Adjust the path as necessary
-app.use("/api/gifts", giftRoutes);
+
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
 
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
@@ -35,11 +33,8 @@ const logger = require("./logger");
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
+app.use("/api/gifts", giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
 
 // Global Error Handler
 app.use((err, req, res, next) => {
